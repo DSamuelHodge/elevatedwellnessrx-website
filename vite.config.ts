0cdcbf63ba9c1b7466a863f3d6f4b55e61c46d8c
DSamuelHodge <dshodge2020@outlook.com>
2025-11-07 18:04:17 -0500

    corrected vite url pointer

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   console.log('Vite mode:', mode);
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'development' ? '/' : '/elevatedwellnessrx-website/',
+    base: mode === 'development' ? '/' : 'https://dsamuelhodge.github.io/elevatedwellnessrx-website/',
     server: {
       port: 3000,
       host: '0.0.0.0',
